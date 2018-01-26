2d0087de4bce08df07b3e3f76f69e460838da2b5
ericwilkening <35818904+ericwilkening@users.noreply.github.com>
2018-01-25 19:23:25 -0600

    Update helloWorld2.c

--- helloWorld2.c
+++ helloWorld2.c
@@ -1,4 +1,8 @@
-
+/**
+ *Author: Sam Curley
+ *Date: 01/26/2018
+ *This Program is for the GitHub Repository
+ **/
 #include<stdio.h>
 int main(int argc, char **argv){
   printf("Sam Curley\nEric Wilkening\n");
